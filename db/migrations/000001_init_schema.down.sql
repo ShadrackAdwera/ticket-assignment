0ce05115c9a638f52e733538277efefa7326c64e
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-25 14:53:39 +0300

    init schema migrations

---
+++ db/migrations/000001_init_schema.down.sql
@@ -0,0 +1,3 @@
+DROP TABLE agents;
+DRoP TABLE tickets;
+DROP TABLE assignments;
---
+++ db/migrations/000001_init_schema.up.sql
@@ -0,0 +1,38 @@
+CREATE TABLE "agents" (
+  "id" bigserial PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "status" varchar NOT NULL DEFAULT 'INACTIVE',
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "tickets" (
+  "id" bigserial PRIMARY KEY,
+  "title" varchar NOT NULL,
+  "description" varchar NOT NULL,
+  "status" varchar NOT NULL DEFAULT 'PENDING',
+  "assigned_to" bigint,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "assignments" (
+  "id" bigserial PRIMARY KEY,
+  "ticket_id" bigint NOT NULL,
+  "agent_id" bigint NOT NULL,
+  "assigned_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "agents" ("name");
+
+CREATE INDEX ON "assignments" ("ticket_id");
+
+CREATE INDEX ON "assignments" ("agent_id");
+
+CREATE INDEX ON "assignments" ("ticket_id", "agent_id");
+
+COMMENT ON COLUMN "agents"."status" IS 'Agent Status can be active or inactive';
+
+ALTER TABLE "tickets" ADD FOREIGN KEY ("assigned_to") REFERENCES "agents" ("id");
+
+ALTER TABLE "assignments" ADD FOREIGN KEY ("ticket_id") REFERENCES "tickets" ("id");
+
+ALTER TABLE "assignments" ADD FOREIGN KEY ("agent_id") REFERENCES "agents" ("id");
