33b85a545feaf052dfe2468606bd92262a835580
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-25 16:58:05 +0300

    config entry point for tests

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -9,20 +9,20 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:password@localhost:5432/simple-bank?sslmode=disable" // TODO: Load via env
-)
-
 var testQuery *Queries
 
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:password@localhost:5432/ticket-assignment?sslmode=disable"
+)
+
 func TestMain(m *testing.M) {
-	sqlDb, err := sql.Open(dbDriver, dbSource)
+	db, err := sql.Open(dbDriver, dbSource)
 
 	if err != nil {
 		log.Fatalf(err.Error())
 		return
 	}
-	testQuery = New(sqlDb)
+	testQuery = New(db)
 	os.Exit(m.Run())
 }
